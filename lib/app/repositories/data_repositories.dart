5c1b5c01b15ee33395a569953c54ff756ac1c4b8
jsimfors <johanna.simfors@gmail.com>
2021-03-31 16:15:28 +0200

    method to get new accesstoken when expired added

--- lib/app/repositories/data_repositories.dart
+++ lib/app/repositories/data_repositories.dart
@@ -1,16 +1,29 @@
 import 'package:corona_stats_app/app/services/api.dart';
 import 'package:corona_stats_app/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
 
 class DataRepository{
   DataRepository({ @required this.apiService});
-  
   final APIService apiService;
 
+  String _accessToken;
   Future<int> getEndpointData(Endpoint endpoint) async {
-    final accessToken = await apiService.getAccessToken();
-    return await apiService.getEndpointData(
-      accessToken: accessToken, endpoint: endpoint
-    );
+  try {
+    if (_accessToken == null){
+      _accessToken = await apiService.getAccessToken();
+    }
+      return await apiService.getEndpointData(
+        accessToken: _accessToken, endpoint: endpoint
+      );
+    } on Response catch (response) {
+      if (response.statusCode == 401){
+        // If the accesstoken has expired
+          _accessToken = await apiService.getAccessToken();
+          return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint
+          );
+      } rethrow; // if we get neither code 200 or 401.
+    }
   }
 } 
