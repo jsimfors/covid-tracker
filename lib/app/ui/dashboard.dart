88fbad1fe74d20106fba8a861be3e8709ffc2b64
jsimfors <johanna.simfors@gmail.com>
2021-04-01 18:46:20 +0200

    API data connected to dahsboard

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,12 +1,28 @@
+import 'package:corona_stats_app/app/repositories/data_repositories.dart';
+import 'package:corona_stats_app/app/services/api.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'endpoint_card.dart';
+
 
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => _DashboardState();
-
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _cases;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    final cases = await dataRepository.getEndpointData(Endpoint.cases);
+    setState(() => _cases = cases);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +30,12 @@ class _DashboardState extends State<Dashboard> {
         title: Text('Coronavirus tracker'),
       ),
       body: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+          EndpointCard(
+            endpoint: Endpoint.cases,
+            value: _cases,
+          ),
+        ],
       ),
     );
   }
--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,26 +1,33 @@
 import 'package:corona_stats_app/app/services/api.dart';
 import 'package:flutter/material.dart';
 
-class EndpointCards extends StatelessWidget {
-  const EndpointCards({Key key, this.endpoint, this.value}) : super(key: key);
+class EndpointCard extends StatelessWidget {
+  const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
   final Endpoint endpoint;
   final int value;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        children: <Widget>[
-          Text('Cases',
-          style: Theme.of(context).textTheme.headline5,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Cases',
+              style: Theme.of(context).textTheme.headline5,
+              ),
+              Text(
+                value != null ? value.toString() : '',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
           ),
-          Text(
-            value != null ? value.toString() : '',
-            style: Theme.of(context).textTheme.display4,
-          ),
-        ],
-      )
+        )
 
+      ),
     );
   }
 }
